489ee6bbccddf7cb2d746076d366be6348fad56d
NV4RE <tosbodesz@gmail.com>
2019-09-27 17:48:13 +0700

    Remove subscript change code

--- 07-admin-start-transaction.js
+++ 07-admin-start-transaction.js
@@ -5,52 +5,55 @@ const config = require("./config.json");
 
 // adminId is require if you wanto subscribe for store event
 const adminClient = new Admin(
-  { ...config.adminConfig, adminId: "test-07-admin-start-transaction" },
+  {
+    ...config.adminConfig
+    //  adminId: "test-07-admin-start-transaction"
+  },
   {}
 );
 
-adminClient.on("TRANSACTION", event => {
-  if (event.isError) {
-    console.log(
-      `transaction: ${event.transactionId} updated rejected`,
-      event.details
-    );
-  } else {
-    console.log(
-      `transaction: ${event.details.transactionId} updated to ${event.details.status}`
-    );
-  }
-});
+// adminClient.on("TRANSACTION", event => {
+//   if (event.isError) {
+//     console.log(
+//       `transaction: ${event.transactionId} updated rejected`,
+//       event.details
+//     );
+//   } else {
+//     console.log(
+//       `transaction: ${event.details.transactionId} updated to ${event.details.status}`
+//     );
+//   }
+// });
 
-adminClient.on("WORKFLOW", event => {
-  if (event.isError) {
-    console.log(
-      `workflow: ${event.transactionId} updated rejected`,
-      event.details
-    );
-  } else {
-    console.log(
-      `workflow: ${event.details.workflowId} updated to ${event.details.status}`
-    );
-  }
-});
+// adminClient.on("WORKFLOW", event => {
+//   if (event.isError) {
+//     console.log(
+//       `workflow: ${event.transactionId} updated rejected`,
+//       event.details
+//     );
+//   } else {
+//     console.log(
+//       `workflow: ${event.details.workflowId} updated to ${event.details.status}`
+//     );
+//   }
+// });
 
-adminClient.on("TASK", event => {
-  if (event.isError) {
-    console.log(`task: ${event.transactionId} updated rejected`, event.details);
-  } else {
-    console.log(
-      `task: ${event.details.taskId} updated to ${event.details.status}`
-    );
-  }
-});
+// adminClient.on("TASK", event => {
+//   if (event.isError) {
+//     console.log(`task: ${event.transactionId} updated rejected`, event.details);
+//   } else {
+//     console.log(
+//       `task: ${event.details.taskId} updated to ${event.details.status}`
+//     );
+//   }
+// });
 
 adminClient.producer.on("ready", () => {
-  setTimeout(() => {
-    console.log('start tasks')
+  // setTimeout(() => {
+    console.log("start tasks");
     for (let i = 0; i < 10; i++) {
       const transactionId = uuid();
-      console.log(`start transaction: ${transactionId}`)
+      console.log(`start transaction: ${transactionId}`);
       adminClient.startTransaction(
         transactionId,
         {
@@ -59,7 +62,7 @@ adminClient.producer.on("ready", () => {
         },
         { hello: "world" }
       );
-      adminClient.subscribe(transactionId);
+      // adminClient.subscribe(transactionId);
     }
-  }, 5000)
+  // }, 5000);
 });
